88c809379252f9dc9561b168a429843b160c49a8
Pakamaru <sanamaru99@gmail.com>
2017-10-19 17:12:31 +0200

    added repository

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+const package = require('./package.json');
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+        if (message.content === 'ping') {
+            message.reply('pong');
+        }
+});
+
+client.login(auth.token);
