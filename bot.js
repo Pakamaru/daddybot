9c7d1e43062c00fe5171fc070979b0125d05f529
Pakamaru <sanamaru99@gmail.com>
2017-10-23 09:49:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,13 +4,21 @@ const auth = require('./auth.json');
 const package = require('./package.json');
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    client.user.setGame("with little girls");
 });
 
-client.on('message', message => {
-        if (message.content === 'ping') {
-            message.reply('pong');
-        }
+client.on('message', async message => {
+    if(message.author.bot) return;
+
+    if(message.content.indexOf(auth.prefix) !== 0) return;
+
+    const args = message.content.slice(auth.prefix.length).trim().split(/ #/g);
+    const command = args.shift().toLowerCase();
+
+    if(command === "ping"){
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
+    }
 });
 
 client.login(auth.token);
