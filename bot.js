1841806da6e0343db87ad470c9441bfabd59e0fe
Pakamaru <sanamaru99@gmail.com>
2017-10-23 11:11:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,26 +9,26 @@ client.on('ready', () => {
 
 client.on('message', async message => {
     if(message.author.bot) return;
-    
+
     if(message.content.indexOf(auth.prefix) !== 0) return;
-    
-    const args = message.content.slice(auth.prefix.length).trim().split(/ #/g);
+
+    const args = message.content.slice(auth.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    
+
     if(command === "ping"){
         const m = await message.channel.send("Ping?");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
     }
-    
+
     if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
+        // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+        // To get the "message" itself we join the `args` back into a string with spaces:
+        const sayMessage = args.join(" ");
+        // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+        message.delete().catch(O_o=>{});
+        // And we get the bot to say the thing:
+        message.channel.send(sayMessage);
+    }
 });
 
 client.login(auth.token);
