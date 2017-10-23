3c6f7ebddcd8066d2a3f2f977d007e1bc7302e52
Pakamaru <sanamaru99@gmail.com>
2017-10-23 10:40:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,18 @@ client.on('ready', () => {
     client.user.setGame("with little girls");
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    message.reply('pong');
-}
+client.on('message', async message => {
+    if(message.author.bot) return;
+    
+    if(message.content.indexOf(auth.prefix) !== 0) return;
+    
+    const args = message.content.slice(auth.prefix.length).trim().split(/ #/g);
+    const command = args.shift().toLowerCase();
+    
+    if(command === "ping"){
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
+    }
 });
 
 client.login(auth.token);
